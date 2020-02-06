cc7d039b356b457c7c91e57e51ef133c9a6b2460
Manoj <manoj.macks08@gmail.com>
2020-02-06 18:44:48 +0530

    files

--- login.js
+++ login.js
@@ -1,18 +1,7 @@
 function sample()
 {
  var x= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})|([0-9]{10})+$/;
-/*  if( document.login.log.value == "")
- {
- 
-   document.getElementById("invalid").innerHTML= "Enter the valid email id or phone";
- } 
- else if(!x.test(document.login.log.value))
- {
- document.getElementById("invalid").innerHTML="Incorrect email or phone";
- }
- else {
- document.getElementById("invalid").innerHTML=" ";
- }      */
+
  if(document.login.pass.value == "mango" && x.test(document.login.log.value))
    {
     window.location.href = "form.html";
@@ -24,4 +13,3 @@ function sample()
     document.getElementById("invalid").innerHTML="Incorrect email or phone";
    }
   } 
- 
